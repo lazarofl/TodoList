6cdc768fb76a40fd875491c04a99feb791a102ba
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-02-09 12:35:52 -0200

    Adicionado uma controller para obter, inserir e alterar o status das todos

---
+++ TodoList/Todo.Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Todo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Add(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Toggle(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
