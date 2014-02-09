5640b36eaad77a3f6f2c5194afae2312d48ac3cf
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-02-09 12:33:50 -0200

    Adicionado o projeto de teste e a estrutura para configurar o activerecord

---
+++ TodoList/Todo.Test/ActiveRecordTestBase.cs
@@ -0,0 +1,46 @@
+﻿using Castle.ActiveRecord;
+using Castle.ActiveRecord.Framework;
+using Castle.ActiveRecord.Framework.Config;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace Todo.Test
+{
+    public abstract class ActiveRecordTestBase
+    {
+        protected SessionScope scope;
+
+        [TestFixtureSetUp]
+        public void InitialiseAR()
+        {
+            ActiveRecordStarter.ResetInitializationFlag();
+            IConfigurationSource source = new
+              XmlConfigurationSource("ActiveRecordConfig.xml");
+            ActiveRecordStarter.Initialize(
+               Assembly.GetAssembly(typeof(Todo.Model.Todo)),
+              source);
+        }
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            ActiveRecordStarter.DropSchema();
+            ActiveRecordStarter.CreateSchema();
+            scope = new SessionScope();
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            scope.Dispose();
+        }
+
+        public void Flush()
+        {
+            scope.Flush();
+            scope.Dispose();
+            scope = new SessionScope();
+        }
+
+    }
+}
---
+++ TodoList/Todo.Test/TodoListTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using Castle.ActiveRecord;
+
+namespace Todo.Test
+{
+    [TestFixture]
+    public class TodoListTests : ActiveRecordTestBase
+    {
+       
+        [Test]
+        public void AdicionarTodoTeste()
+        {
+            Todo.Model.Todo todo = new Model.Todo();
+            todo.Name = "Nova todo";
+            todo.Save();
+
+            var expected = 1;
+
+            var result = Todo.Model.Todo.FindAll().Length;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void MarcarComoFinalizadoTeste()
+        {
+
+        }
+
+        [Test]
+        public void ObterTodasAsTodosTeste()
+        {
+
+        }
+
+
+    }
+}
