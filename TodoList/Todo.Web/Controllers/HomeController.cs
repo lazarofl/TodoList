94e0579c6930cccbf0b54f5b82fdebd4fc1b7cef
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-02-09 15:17:27 -0200

    Controller default contendo as ações de crud

--- TodoList/Todo.Web/Controllers/HomeController.cs
+++ TodoList/Todo.Web/Controllers/HomeController.cs
@@ -10,19 +10,35 @@ namespace Todo.Web.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult GetAll()
         {
-            throw new NotImplementedException();
+            var todolist = Todo.Model.Todo.FindAll();
+            return Json(todolist, JsonRequestBehavior.AllowGet);
         }
 
+        [HttpPost]
         public ActionResult Add(string name)
         {
-            throw new NotImplementedException();
+            var todo = new Todo.Model.Todo();
+            todo.Name = name;
+            todo.Done = false;
+
+            todo.SaveAndFlush();
+
+            return Json(todo, JsonRequestBehavior.AllowGet);
+
         }
 
-        public ActionResult Toggle(string id)
+        [HttpPost]
+        public ActionResult Done(string id, bool done)
         {
-            throw new NotImplementedException();
+            var todo = Todo.Model.Todo.Find(int.Parse(id));
+            todo.Done = done;
+
+            todo.SaveAndFlush();
+
+            return Json(todo, JsonRequestBehavior.AllowGet);
         }
 
     }
