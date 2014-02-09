f65453e9367982491eb9ea74cf5c7d6f6584920b
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-02-09 15:15:06 -0200

    Css para centralizar a todo list

---
+++ TodoList/Todo.Web/assets/angularapps/TodoApp.js
@@ -0,0 +1,24 @@
+﻿function TodoListController($scope, $http) {
+
+    $scope.todoList = [];
+
+    $scope.GetAll = function () {
+        $http.get('/Home/GetAll').success(function (todolist) {
+            console.log(todolist);
+            $scope.todoList = todolist;
+        });
+    }
+
+    $scope.Add = function () {
+        $http.post('/Home/Add', { name: $scope.name }).success(function (todo) {
+            $scope.todoList.splice(0, 0, todo);
+            $scope.name = "";
+        });
+    }
+
+    $scope.Toggle = function (item) {
+        $http.post('/Home/Done', { id: item.Id, done: !item.Done });
+    }
+
+
+}
