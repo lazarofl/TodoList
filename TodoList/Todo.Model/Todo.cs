0149758072b5f2a794adc897b3c7ec8d9d841906
Lazaro Fernandes Lima <lazaro.fl@gmail.com>
2014-02-09 12:12:29 -0200

    Projeto modal

---
+++ TodoList/Todo.Model/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.ActiveRecord;
+
+namespace Todo.Model
+{
+    [ActiveRecord(Table = "todo")]
+    public class Todo : ActiveRecordValidationBase<Todo>
+    {
+        [PrimaryKey(Column = "Id")]
+        public virtual int Id { get; set; }
+
+        [Property(Column = "Name", NotNull = true)]
+        public virtual string Name { get; set; }
+
+        [Property(Column = "Done", NotNull = true)]
+        public virtual string Done { get; set; }
+    }
+}
